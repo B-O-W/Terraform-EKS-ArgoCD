57b1805d873c9a7a6b63cb6be1b05de93e9606e3
Elbrus Mammadov <95022459+B-O-W@users.noreply.github.com>
2022-08-08 17:12:09 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,7 +25,8 @@ function deployArgoCD() { # Deploy ArgoCd
     kubectl create namespace argocd
     kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
     kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-    kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; |echo "  It's password:"
     kubectl apply -f argocd/
+    kubectl get svc -n argocd | grep argocd-server | head -n 1 | awk '{print$4}'; echo "  Go to this link it's alb default user admin"
+    kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; |echo "  It's password:"
 }
 main
